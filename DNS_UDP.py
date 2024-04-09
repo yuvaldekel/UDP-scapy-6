5ef5b76ae8edad83c138d4c7ea1b625c4b3366d1
Yuval Dekel <yuvddd05@gmail.com>
2024-04-09 11:39:05 +0300

    send query sniff reply

--- DNS_UDP.py
+++ DNS_UDP.py
@@ -2,8 +2,10 @@ from scapy.all import IP, UDP, TCP, DNS, DNSQR, DNSRR
 from scapy.all import *
 
 def main():
-    dns_packet = IP(dst = '8.8.8.8')/UDP(sport = 24601,dport = 53)/DNS(rd = 0, qdcount = 1,qd = DNSQR(qname='www.google.com.')) 
-    send(dns_packet)
+    query_packet = IP(dst = '8.8.8.8')/UDP(sport = 24601,dport = 53)/DNS(rd = 1, qdcount = 1,qd = DNSQR(qname='www.amazon.com.')) 
+    print(query_packet.show())
+    reply_packet = sr1(query_packet)
+    print(reply_packet.show())
     
 if __name__ == '__main__':
     main()
